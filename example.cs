5361f7c2fa4fe06c7133bfd79c0fadb23342ead4
Robert Rumney <robertrumney@gmail.com>
2023-04-16 16:07:25 +0200

    Create example.cs

---
+++ example.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        // Subscribe to the onClick and onDoubleClick events
+        MouseClicks.onClick.AddListener(HandleClick);
+        MouseClicks.onDoubleClick.AddListener(HandleDoubleClick);
+    }
+
+    private void HandleClick()
+    {
+        Debug.Log("A proper mouse click occurred.");
+    }
+
+    private void HandleDoubleClick()
+    {
+        Debug.Log("A proper double click occurred.");
+    }
+}
